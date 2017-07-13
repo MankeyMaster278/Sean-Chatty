c9fb64d3615cb1ee81d7e29c8895df6c8f9517e0
Sean <Smlyn704@gmail.com>
2017-07-13 15:48:44 -0500

    Hi

---
+++ Js.js
@@ -0,0 +1,33 @@
+var box = document.querySelector('div#box');
+
+box.insertAdjacentHTML("afterbegin", 
+`<p class='green Welcome to life chat`);
+
+var button = document.querySelector('button');
+var inputbox =document.querySelector('input')
+
+button.addEventListener( 'click', function() {
+    console.log(inputbox.value)
+
+    box.insertAdjacentHTML("beforeend", "<p>" + inputbox.value + "</p>");
+
+     box.scrollTo(0,Box.scrollHeight);
+
+    mascotFeedback();
+})
+
+function mascotFeedback(){
+    
+
+    setTimeout(function() { 
+        box.insertAdjacentHTML( "beforeend", `<p> I'm ${mascot.name} </p>`);
+    }, 3000)
+
+}
+
+
+var mascot = {
+    name: "reverseFlash",
+    age: 26
+
+}
